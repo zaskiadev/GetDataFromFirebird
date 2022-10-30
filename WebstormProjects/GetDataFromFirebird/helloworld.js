7a27fcef7f7a1f198ce9125de481f149a8df60d2
zaskiadev <fandy.sp@gmail.com>
2022-10-31 00:30:01 +0700

    parse-body-form

--- WebstormProjects/GetDataFromFirebird/helloworld.js
+++ WebstormProjects/GetDataFromFirebird/helloworld.js
@@ -13,9 +13,10 @@ http.createServer(function (req, res) {
 const express = require('express');
 const exphbs = require('express-handlebars');
 var firebird = require('node-firebird');
+const Console = require("console");
 const app = express();
 var options={};
-options.host = '127.0.0.1';
+options.host = '192.168.9.79';
 options.port = 3050;
 options.database = '/opt/firebird/examples/empbuild/employee.fdb';
 options.user = 'SYSDBA';
@@ -25,6 +26,9 @@ options.role = null;            // default
 options.pageSize = 4096;        // default when creating database
 options.pageSize = 4096;        // default when creating database
 options.retryConnectionInterval = 1000; // reconnect interval in case of connection drop
+
+var bodyparser= require('body-parser')
+
 //setup templating
 app.engine('hbs', exphbs.engine({
     layoutsDir: __dirname + '/views',
@@ -33,10 +37,10 @@ app.engine('hbs', exphbs.engine({
     partialsDir: __dirname + '/views/partials/'
 
 }));
-
+var jsonParser = bodyparser.json()
+var urlencodedParser = bodyparser.urlencoded({extended:false})
 //CSS location
-app.use(express.static('public'))
-
+app.use(express.static('public'));
 app.set('view engine', 'hbs');
 
 //app navigation
@@ -53,25 +57,55 @@ var resultDB;
         if (err)
             throw err
 
-        db.query('SELECT EMP_NO,FULL_NAME,PHONE_EXT,HIRE_DATE,SALARY  FROM EMPLOYEE', function(err, result) {
+        db.query('SELECT EMP_NO,FULL_NAME,PHONE_EXT,HIRE_DATE,SALARY  FROM EMPLOYEE', function(err, resultData) {
             // IMPORTANT: close the connection
-        resultDB=result;
+          //  console.log(result)
+            //console.log(resultData);
+            res.render('data_employee', {resultData:resultData
+
+            });
             db.detach();
         });
 
     });
 
-    res.render('data_employee', {dataDB:resultDB
+    //console.log(resultDB)
 
-    });
 });
 
 app.get('/add_employee', function (req, res) {
-    res.render('add_employee', {
+    firebird.attach(options,function (err, db){
+        if (err)
+            throw err
+
+        db.query('SELECT first 1 EMP_NO FROM EMPLOYEE order by EMP_NO desc', function(err, resultData) {
+            // IMPORTANT: close the connection
+            //  console.log(result)
+            //console.log(resultData);
+            var dataKirim=resultData[0]['EMP_NO']+1
+
+            res.render('add_employee', {dataKirim:dataKirim
+
+            });
+            db.detach();
+        });
+
+    });
+
+});
+
+app.post('/add_employee', urlencodedParser, function (req, res) {
+
+    const data= req.body
+   // console.log(data.FirstName)
+    var FirstName= data.FirstName;
+    var LastName= data.LastName;
+
+    res.render('home', {
 
     });
 });
 
-app.listen(8888, () => {
+app.listen(8877, () => {
     console.log('The web server has started on port 8888');
 });
